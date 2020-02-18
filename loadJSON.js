487d0207c2c9e7294018d87bb75dbb9e97505b52
Bykov Alexander <bykov@Bykovs-MacBook-Pro.local>
2020-02-18 11:58:47 +0300

    - Все строки вынесены в JSON-файлы - Вакансии подгружаются из workable - Рефакторинг немношк

--- loadJSON.js
+++ loadJSON.js
@@ -1,22 +1,38 @@
 (() => {
-    const jsons = document.querySelectorAll('script[jsonObject],script[jsonCallback]');
-    for (const i in jsons) {
-        if (!jsons[i] || !jsons[i].getAttribute) continue;
-        const j = jsons[i];
-        const jsonPath = j.getAttribute('src');
-        const objName = j.getAttribute('jsonObject');
-        const callbackName = j.getAttribute('jsonCallback');
-        const callback = window[callbackName] || null;
+  const jsons = document.querySelectorAll('script[json]');
+  window.awaitJSON = window.awaitJSON || {};
+  window.getLoadedJSON = (objectName) => {
+    if (objectName in window.awaitJSON)
+      return window.awaitJSON[objectName];
+    return Promise.resolve(window[objectName])
+  };
+  for (const i in jsons) {
+    if (!jsons[i] || !jsons[i].getAttribute) continue;
+    const j = jsons[i];
+    const jsonPath = j.getAttribute('json');
+    const objName = j.getAttribute('jsonObject');
+    const callbackName = j.getAttribute('jsonCallback');
+    const callback = window[callbackName] || null;
 
-        fetch(jsonPath)
-            .then(response => response.json())
-            .then(json => {
-                if (typeof callback === 'function')
-                    callback(json);
-                if (objName)
-                    window[objName] = json;
-            })
-            // eslint-disable-next-line no-console
-            .catch(console.error);
+    if (!objName && !callbackName)
+      continue;
+
+    const fetchPromise = fetch(jsonPath)
+      .then(response => response.json())
+      .then(json => {
+        deleteAwaiter()
+        if (typeof callback === 'function')
+          callback(json);
+        if (objName)
+          window[objName] = json;
+        return json;
+      })
+      // eslint-disable-next-line no-console
+      .catch(console.error);
+
+    window.awaitJSON[objName || callbackName] = fetchPromise
+    const deleteAwaiter = () => {
+      delete window.awaitJSON[objName || callbackName]
     }
+  }
 })()
